405757022f9549cebd7be3ae8ac15fc5f9dccb05
meiyi <50971932+liazhuzhu@users.noreply.github.com>
2020-09-16 16:10:03 +0800

    优化算法后出现bug
    F值不随方案变化而变化

--- mofan/Reinforcement-learning-with-tensorflow/contents/Q_Learnig_max/main.py
+++ mofan/Reinforcement-learning-with-tensorflow/contents/Q_Learnig_max/main.py
@@ -1,24 +1,50 @@
 import system_model
-# 2020/9/13
-my_model = system_model.System_model(2) #Create an object of the class, the total bandwidth is 2 MHz
+# 2020/9/16
+my_model = system_model.System_model(2e6) #Create an object of the class, the total bandwidth is 2 MHz
 
 print(1)
-first_F =  my_model.F
-best_F = my_model.F
-for episode in range(100):
-    while True:
-        # choose action from current state
+first_F =  my_model.F #the original F
+best_F = my_model.F #the original F
+best_Bm = my_model.Bm # the original Bm[]
+
+for episode in range(10):
+    ###while True:
+    
+        # debug vaneriables
+        watch_F = my_model.F
+        
+        #(1).Use current state and q_table to choose action(string) 
         action = my_model.choose_action(str(my_model.Bm))
-        state_last, reward, if_restart = my_model.step(action)
+       
+        #(2).Use action(string) and present state to get the next state and reward
+        state_last, reward, if_below_limit, state_last_list = my_model.step(action)
+
+        # debug vaneriables
+        watch_delta_F = my_model.F - watch_F
+
+        #Record the highest F value and Bm[]
+        #The self.parameters are global variables which is changinng during the whole process
         if my_model.F > best_F:
             best_F = my_model.F
-        
+            best_Bm = my_model.Bm
+
+        #(3).Use s, a, s_, r to update the q table  
         my_model.learn(state_last, action, reward, str(my_model.Bm))
-        if if_restart:
-            my_model.restart()
-            print(best_F)
-            #print(my_model.F)
-            print("episode end")
-            break
+        
+        #This step must behind the update_q_table, 
+        #because the state that is below the limit should also be recorded in the q table
+        if if_below_limit:
+            my_model.Bm = state_last_list
+        
+        ###print(best_F)
+        ###print(best_Bm)
+        ###print("episode end")
+        
+        ###if if_restart:
+        ###    my_model.restart()
+        ###    print(best_F)
+        ###    print("episode end")
+        ###    break
         
 print("*****     " , best_F - first_F)
+print(best_Bm)
--- mofan/Reinforcement-learning-with-tensorflow/contents/Q_Learnig_max/system_model.py
+++ mofan/Reinforcement-learning-with-tensorflow/contents/Q_Learnig_max/system_model.py
@@ -8,7 +8,7 @@ import math
 import itertools
 class System_model:
     #initialize parameters
-    def __init__(self, B_total = 2):
+    def __init__(self, B_total=2e6): # total band resource is a variable parameter, range from 2 to 20 MHz
         self.M = 3 # numbers of vehicles
         self.N = 3 # numbers of categories
 
@@ -16,38 +16,39 @@ class System_model:
         for n in range(self.N):
             Delta.append(1)
         self.Delta = Delta
-
-        V = [] # vectors of the speed of the N vehicles, slower than 60km/h
-        for n in range(self.M):
-            V.append(random.randint(0,60))
-        self.V = V
+        
+        ###Vm are used in calculating the small-scale fading by independent first-order autoregressive processes
+        ###V = [] # vectors of the speed of the N vehicles, slower than 60km/h
+        ###for n in range(self.M):
+        ###    V.append(random.randint(0,60))
+        ###self.V = V
         
         D = [] # vectors of the distance from each vehicles to the server; range from 0.1km to 1.1km
-        for n in range(self.M):
+        for m in range(self.M):
             D.append(random.uniform(0.1,1.1))
         self.D = D
         
         S = [] # vectors of the transmit power for each vehicle; all 23dBm
-        for n in range(self.M): 
-            S.append(23)
+        for m in range(self.M): 
+            S.append(math.pow(10 , 23/10.0))
         self.S = S
         
         Sigma_square = [] # vectors of the noise power for each vehicles; all -174 dBm/Hz
-        for n in range(self.M):
-            Sigma_square.append(-174)
+        for m in range(self.M):
+            Sigma_square.append(math.pow(10 , -174/10.0))
         self.Sigma_square = Sigma_square
         
-        self.fc = 2 # center frequency of the band; 2GHz
+        ###self.fc = 2 # center frequency of the band; 2GHz
 
-        self.delta_t = 200 # processing delay; 200ms
+        ###self.delta_t = 200 # processing delay; 200ms
 
-        self.te = 50 # small-scaling fading interval; 50ms
+        ###self.te = 50 # small-scaling fading interval; 50ms
 
-        self.B_total = B_total # total band resourse; MHz 2~20; it's a variable parameter
+        self.B_total = B_total # total band resource; MHz 2~20; it's a variable parameter
         
         self.B_min = B_total/10.0 #minimum bandwidth limit
         
-        self.P_min = 0.3 # minimum detection accuracy
+        ###self.P_min = 0.3 # minimum detection accuracy
 
         self.stdShadow = 8 # standard deviation; dB
         
@@ -74,37 +75,35 @@ class System_model:
         #Q_learning parameters
         actions_tmp = []
         actions_str_tmp = ''
-<<<<<<< HEAD
-        for i in range(3):
-            actions_str_tmp = actions_str_tmp + str(i)
-=======
-        for i in range(self.M):
-                    actions_str_tmp = actions_str_tmp + str(i)
->>>>>>> edc71511e08ff1cc6d1d7e726187524ba94d0f91
+
+        for i in range(self.M): #Create a string of actions
+            actions_str_tmp = actions_str_tmp + str(i) #'012'
         
-        for i in itertools.combinations(actions_str_tmp, 2):
-            actions_tmp.append(''.join(i))
-            actions_tmp.append(''.join(i[::-1]))  #Concatenate string array；'' is delimiter
-        self.actions = actions_tmp
+        #Create the colums(list of actions) of q_table by permutations
+        ###for i in itertools.combinations(actions_str_tmp, 2):
+        for i in itertools.permutations(actions_str_tmp, 2):
+             #i = ('0', '1')('0', '2')('1', '0')('1', '2')('2', '0')('2', '1')
+             #() is tuple, [] is list. Tupel's elements must be the same type.
+            actions_tmp.append(''.join(i)) #Concatenate string array；'' is delimiter
+            ###actions_tmp.append(''.join(i[::-1])) #Copy i but inverse 
+        self.actions = actions_tmp #action is a list of string actions = ['01','02','10','12','20','21']
 
         self.epsilon = 0.9 #greedy police
         self.q_alpha = 0.1 #learning rate
         self.q_gamma = 0.9 #discount factor
         self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64) #build an empty q_table
-<<<<<<< HEAD
+
         self.delta_B = B_total/100.0 #minimum change of the bandwidth (one step change)
-
-        # F calculation
+       
+        # F calculation  
         self.F = self.calculate_F()
-=======
-        self.delta_B = B_total/30.0 #minimum change of the bandwidth
->>>>>>> edc71511e08ff1cc6d1d7e726187524ba94d0f91
-
-
+    
+    """Functions"""
     #Calculate the total fading
     def calculate_fading(self, stdShadow, dist):
-        h_pl_db = 148.1 + 37.6 * math.log10(dist) #Get the path loss, which is usually minus
-        h_sh_db = np.random.randn(1) * stdShadow #Get the shadowing(log-normal distribution)
+        h_pl_db = 148.1 + 37.6 * math.log10(dist) #Get the path loss, which is usually minus(dB)
+        h_sh_db = np.random.randn() * stdShadow #Get the shadowing(log-normal distribution)(dB)
+        #TO get the normal distribution with mean u and variance x²: x*np.random.randn()+u
 
         #The envelope of the sum of two orthogonal Gaussian noise signals obeys the Rayleigh distribution.
         #Get two orthogonal Gaussian functions,one is the real part, and another is the imaginary part.
@@ -119,7 +118,7 @@ class System_model:
     #Calculate the transfer rate: RTm=Bm*log2(1+Sm*hm/sigma²)
     def calculate_R(self, m):
         #trasfer rate
-        Rm = self.Bm[m] * 10e6 *math.log2(1+self.S[m]*self.calculate_fading(self.stdShadow,self.D[m])/self.Sigma_square[m])
+        Rm = self.Bm[m]*math.log2(1+self.S[m]*self.calculate_fading(self.stdShadow,self.D[m])/self.Sigma_square[m])
         return Rm
     
     #Calculate the QP value of the video encoder: Qm=am*exp(bm*Rm) 
@@ -144,60 +143,66 @@ class System_model:
         F = F/self.M
         return F
     
-    ######Use Q_learning to get the maximum of F
-    #Enlarge the Q_Table's states
+
+    """Use Q_learning to get the maximum of F"""
+    
+    #Enlarge the Q_Table's states.
     def check_state_exist(self, state):
-        if state not in self.q_table.index:
+        if state not in self.q_table.index: #Index is the states, while the column is the actions 
             # append new state to q table
             self.q_table = self.q_table.append(pd.Series([0]*len(self.actions), index=self.q_table.columns, name=state))
 
-    #Choose to increase or decrease(the best action or random)
-    def choose_action(self, observation):
+    #Choose actions (the best action(90%) or random(10%))
+    def choose_action(self, observation): #observation is the present state
         self.check_state_exist(observation)
         if np.random.uniform() < self.epsilon:
             # choose the best action
             state_action = self.q_table.loc[observation, :]
-            # some actions may have the same value, randomly choose on in these actions
+            # some actions may have the same value, randomly choose one in these actions
             action = np.random.choice(state_action[state_action == np.max(state_action)].index)
         else:
             # choose random action
             action = np.random.choice(self.actions)
-        return action
-
-    #update the q_table
-    def learn(self, s, a, r, s_):
-        self.check_state_exist(s_)
-        q_predict = self.q_table.loc[s, a]
-        if s_ != 'terminal':
-            q_target = r + self.q_gamma * self.q_table.loc[s_, :].max()  # next state is not terminal
-        else:
-            q_target = r  # next state is terminal
-        self.q_table.loc[s, a] += self.q_alpha * (q_target - q_predict)  # update
+        return action  #'01' '10' '02' '20' '12' '21'
 
     # update state
     def step(self, action):
-        if_restart = False
-        state_last = str(self.Bm)
+        if_below_limit = False
+        state_last = str(self.Bm) #return the last state to the update_q_table function 
+        ##try to not change the last state
+        state_last_list = self.Bm #Save the last state to avoid the next state doesn't meet the condition.
         reward = 0
-        # action is a 1 times 2 vecter(list), first is add second is -
+        #Action is like '01' '10' '02' '20' '12' '21', action[0] is addtion; while action[1] is subtraction
         self.Bm[int(action[0])] = self.Bm[int(action[0])] + self.delta_B
         self.Bm[int(action[1])] = self.Bm[int(action[1])] - self.delta_B
-        if self.Bm[int(action[1])] < self.B_min:
+        if self.Bm[int(action[1])] < self.B_min:  #The restrictions include the minimum limit of Bm
             reward = -100
-            if_restart = True
+            if_below_limit = True
+
         else:
             F_new = self.calculate_F()
             reward = F_new - self.F
             self.F = F_new
 
-        return state_last, reward, if_restart
+        return state_last, reward, if_below_limit, state_last_list
     
-    def restart(self):
-        for i in range(self.M):
-            self.Bm[i] = self.B_total / self.M
-            self.F = self.calculate_F()
-        return
-
+    ####Back to the initialized state
+    ###def restart(self):
+    ###    for i in range(self.M):
+    ###        self.Bm[i] = self.B_total / self.M
+    ###        self.F = self.calculate_F()
+    ###    return
+        
+    #update the q_table
+    def learn(self, s, a, r, s_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s, a]
+        #There is no terminal, since we hope the F could be higher.
+        ###if s_ != 'terminal':
+        q_target = r + self.q_gamma * self.q_table.loc[s_, :].max()  # next state is not terminal
+        ###else:
+        ###    q_target = r  # next state is terminal
+        self.q_table.loc[s, a] += self.q_alpha * (q_target - q_predict)  # update
      
     
 
